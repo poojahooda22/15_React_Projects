d1cc8b6195fdd2ca7a606df026b974df31214ea5
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-19 23:54:38 +0530

    10-grocery
    Second-commit

--- 10-grocery-card/src/App.js
+++ 10-grocery-card/src/App.js
@@ -9,10 +9,24 @@ function App() {
   const[editID, setEditID] = useState(null);
   const[alert, setAlert] = useState({show: false, msg:'', type:''});
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
+
 
   return (
     <section className="section-center">
-      <form></form>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert/>}
+        <h3>Grocery Bud</h3>
+        <div className='form-control'>
+          <input type="text"/>
+          <button type="submit" className='submit-btn'>
+            {isEditing ? 'edit': 'submit'}
+          </button>
+        </div>
+      </form>
       <div className='grocery-container'>
         <List/>
         <button className='clear-btn'>Clear Items</button>
