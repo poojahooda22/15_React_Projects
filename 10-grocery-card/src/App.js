c706ba7cf549ecf19b3e270a3f14731b4baa46e1
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-19 23:12:04 +0530

    10-started
    first-commit

---
+++ 10-grocery-card/src/App.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from 'react';
+import List from './List';
+import Alert from './Alert'
+
+function App() {
+  const[name, setName] = useState('');
+  const[list, setList] = useState([]);
+  const[isEditing, setIsEditing] = useState(false);
+  const[editID, setEditID] = useState(null);
+  const[alert, setAlert] = useState({show: false, msg:'', type:''});
+
+
+  return (
+    <section className="section-center">
+      <form></form>
+      <div className='grocery-container'>
+        <List/>
+        <button className='clear-btn'>Clear Items</button>
+        </div>
+    </section>
+  );
+}
+
+export default App;
