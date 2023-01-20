b53ace1d3acb018e22cd22352b9c73fd18809f61
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-20 23:03:55 +0530

    10-grocery
    Third commit

--- 10-grocery-card/src/App.js
+++ 10-grocery-card/src/App.js
@@ -11,7 +11,18 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello')
+    if(!name) {
+      //display with alert
+
+    }else if(name) {
+      //deal with edit
+
+    }
+    else{
+      //show alert
+      const newItem = {id: new Date().getTime().toString(), title:name};
+      setList([...list, newItem]);
+    }
   }
 
 
@@ -19,16 +30,16 @@ function App() {
     <section className="section-center">
       <form className='grocery-form' onSubmit={handleSubmit}>
         {alert.show && <Alert/>}
-        <h3>Grocery Bud</h3>
+        <h3>Add items to the list</h3>
         <div className='form-control'>
-          <input type="text"/>
+          <input type="text" className='grocery' placeholder="eg. eggs" value={name}/>
           <button type="submit" className='submit-btn'>
             {isEditing ? 'edit': 'submit'}
           </button>
         </div>
       </form>
       <div className='grocery-container'>
-        <List/>
+        <List items={list}/>
         <button className='clear-btn'>Clear Items</button>
         </div>
     </section>
