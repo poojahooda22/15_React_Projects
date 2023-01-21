e1cbd6033c6a975b8036a58e23cadcd312a2ba38
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-21 07:26:24 +0530

    10-grocery
    final-changes

--- 10-grocery-card/src/Alert.js
+++ 10-grocery-card/src/Alert.js
@@ -1,16 +1,13 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 
-const Alert = ({type, msg, removeAlert, list}) => {
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-          removeAlert();
-        }, 3000);
-        return () => clearTimeout(timeout);
-      }, [list]);
-    return (
-        <p className={`alert alert-${type}`}>{msg}</p>
-
-    )
+const Alert = ({ type, msg, removeAlert, list }) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [list]);
+  return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
-export default Alert
+export default Alert;
--- 10-grocery-card/src/List.js
+++ 10-grocery-card/src/List.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {FaEdit, FaTrash} from 'react-icons/fa'
 
-const List = ({items}) => {
+const List = ({items, removeItem, editItem}) => {
     return (
         <div className='grocery-list'>
             {items.mao((item) => {
@@ -10,15 +10,15 @@ const List = ({items}) => {
                     <article key={id} className="grocery-item">
                         <p className='title'>{title}</p>
                         <div className='btn-container'>
-                            <button type="button" className='edit-btn'>
+                            <button type="button" className='edit-btn' onClick={() => editItem(id)}>
                                 <FaEdit/>
                             </button>
-                            <button type="button" className='delete-btn'>
+                            <button type="button" className='delete-btn' onClick={() => removeItem(id)}>
                                 <FaTrash/>
                             </button>
                         </div>
                     </article>
-                )
+                );
             })}
         </div>
     )
