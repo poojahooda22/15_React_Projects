a8e58c0e9e294f171aa449ef1cd406e83a7b653e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-25 23:13:29 +0530

    updated

--- 12-sidebar/src/Sidebar.js
+++ 12-sidebar/src/Sidebar.js
@@ -27,6 +27,16 @@ const Sidebar = () => {
           );
         })}
       </ul>
+      <ul className='social-icons'>
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
         
         
       </aside>
