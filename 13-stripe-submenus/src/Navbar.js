0b617c8468d4dab68b2d60a135d1a67e9e38d108
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-27 23:29:13 +0530

    Create Navbar

---
+++ 13-stripe-submenus/src/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from './logo.svg'
+import {FaBars} from 'react-icons/fa'
+import {useGlobalContext} from './context'
+
+const Navbar = () => {
+    return <nav className="nav">
+        <div className='nav-center'>
+            <div className='nav-header'>
+
+            </div>
+
+        </div>
+    </nav>
+
+}
+
+export default Navbar;
