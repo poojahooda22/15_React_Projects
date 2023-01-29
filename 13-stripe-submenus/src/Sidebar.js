a9f6ca372785b364c531e426819a9a7d62c289b3
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-29 23:26:27 +0530

    update sidebar

--- 13-stripe-submenus/src/Sidebar.js
+++ 13-stripe-submenus/src/Sidebar.js
@@ -6,11 +6,40 @@ import { useGlobalContext} from './context'
 const Sidebar = () => {
     const {isSidebarOpen, closeSidebar} =useGlobalContext()
 
-    return ( 
-        <h2>
-            Sidebar Component
-        </h2>
-    )
+    return (
+        <div
+          className={`${
+            isSidebarOpen ? 'sidebar-wrapper show' : 'sidebar-wrapper'
+          }`}
+        >
+          <aside className='sidebar'>
+            <button className='close-btn' onClick={closeSidebar}>
+              <FaTimes />
+            </button>
+            <div className='sidebar-links'>
+              {sublinks.map((item, index) => {
+                const { links, page } = item
+                return (
+                  <article key={index}>
+                    <h4>{page}</h4>
+                    <div className='sidebar-sublinks'>
+                      {links.map((link, index) => {
+                        const { url, icon, label } = link
+                        return (
+                          <a key={index} href={url}>
+                            {icon}
+                            {label}
+                          </a>
+                        )
+                      })}
+                    </div>
+                  </article>
+                )
+              })}
+            </div>
+          </aside>
+        </div>
+      )
 }
 
 export default Sidebar;
