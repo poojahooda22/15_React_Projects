060b8fb45ecd64cd4806a157b08c88548ccb67f0
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-01-30 22:34:15 +0530

    Update navbar

--- 13-stripe-submenus/src/Navbar.js
+++ 13-stripe-submenus/src/Navbar.js
@@ -5,7 +5,19 @@ import { useGlobalContext} from './context'
 
 
 const Navbar = () => {
-    const{ openSider, openSubmenu, closeSubmenu } = useGlobalContext()
+    const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+    const displaySubmenu = (e) => {
+      const page = e.target.textContent;
+      const tempBtn = e.target.getBoundingClientRect();
+      const center = (tempBtn.left + tempBtn.right) / 2;
+      const bottom = tempBtn.bottom - 3;
+      openSubmenu(page, { center, bottom });
+    };
+    const handleSubmenu = (e) => {
+      if (!e.target.classList.contains('link-btn')) {
+        closeSubmenu();
+      }
+    };
     return (
         <nav className="nav">
             <div className='nav-center'>
