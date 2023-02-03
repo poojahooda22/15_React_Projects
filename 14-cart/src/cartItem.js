eb5bc3f6ab8079e1fa5fae8aab633974e21e63c6
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-03 22:58:23 +0530

    update cartItem

--- 14-cart/src/cartItem.js
+++ 14-cart/src/cartItem.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { useGlobalContext } from './context'
-
 const CartItem = ({ id, img, title, price, amount }) => {
-    
-    return (
-      <article className='cart-item'>
-        <img src={img} alt={title} />
-        <div>
+  const { remove, increase, decrease, toggleAmount } = useGlobalContext()
+  return (
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
         <h4>{title}</h4>
         <h4 className='item-price'>${price}</h4>
         {/* remove button */}
@@ -30,8 +29,8 @@ const CartItem = ({ id, img, title, price, amount }) => {
           </svg>
         </button>
       </div>
-      </article>
-    )
-  }
-  
-  export default CartItem
+    </article>
+  )
+}
+
+export default CartItem
