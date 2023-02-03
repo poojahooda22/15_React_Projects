bce8003d17ec72a1943ba3fe4ef89c878a31a25f
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-03 22:57:16 +0530

    update context

---
+++ 14-cart/src/context.js
@@ -0,0 +1,11 @@
+import React, { useState, useContext, useReducer, useEffect } from 'react'
+import cartItems from './data'
+import reducer from './reducer'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-useReducer-cart-project'
+const AppContext = React.createContext()
+
+
+
+export { AppContext, AppProvider }
