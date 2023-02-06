70c382e0b771c426737ebf508de01db0e8595096
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-06 22:51:55 +0530

    update context

--- 15-cocktails/src/context.js
+++ 15-cocktails/src/context.js
@@ -1,2 +1,9 @@
 import React, { useState, useContext, useEffect } from 'react'
 import { useCallback } from 'react'
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
+const AppContext = React.createContext()
+
+
+
+export { AppContext, AppProvider }
