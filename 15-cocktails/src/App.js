ee8d8760a02ef47d142c320880de022498d13ffe
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-07 23:40:49 +0530

    Update App

--- 15-cocktails/src/App.js
+++ 15-cocktails/src/App.js
@@ -4,3 +4,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
+
+import Error from './pages/Error';
+// import components
+import Navbar from './components/Navbar';
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />} />
+        <Route path='cocktail/:id' element={<SingleCocktail />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
