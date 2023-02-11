cf174aa6a05b0def3847ef6796b83dac9731051c
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-11 23:30:38 +0530

    update context

--- 15-cocktails/src/context.js
+++ 15-cocktails/src/context.js
@@ -4,6 +4,15 @@ import { useCallback } from 'react'
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
-
+const AppProvider = ({ children }) => {
+    const [loading, setLoading] = useState(true)
+    const [searchTerm, setSearchTerm] = useState('a')
+    const [cocktails, setCocktails] = useState([])
+  
+    
+  // make sure use
+  export const useGlobalContext = () => {
+    return useContext(AppContext)
+  }
 
 export { AppContext, AppProvider }
