69ef5c880986e369a94bf958564a682c2e00d3df
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-15 23:24:53 +0530

    updat APp

--- 17-dark-mode/src/App.js
+++ 17-dark-mode/src/App.js
@@ -10,5 +10,37 @@ const getStorageTheme = () => {
   return theme;
 };
 
+function App() {
+  const [theme, setTheme] = useState(getStorageTheme());
 
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>overreacted</h1>
+          <button className="btn" onClick={toggleTheme}>
+            toggle
+          </button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
+}
 export default App;
