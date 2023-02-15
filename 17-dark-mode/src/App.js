0ddd28408483c263e86bfa7f07923c6539a475cb
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-15 23:24:30 +0530

    update App

--- 17-dark-mode/src/App.js
+++ 17-dark-mode/src/App.js
@@ -2,4 +2,13 @@ import React, { useState, useEffect } from 'react';
 import data from './data';
 import Article from './Article';
 
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
+
+
 export default App;
