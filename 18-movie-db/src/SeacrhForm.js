b69a33262dd543d418df15dc338331befaa2c2f5
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-02-17 23:50:22 +0530

    update searchForm

--- 18-movie-db/src/SeacrhForm.js
+++ 18-movie-db/src/SeacrhForm.js
@@ -5,7 +5,12 @@ const SearchForm = () => {
   return (
     <form className='search-form' onSubmit={(e) => e.preventDefault()}>
       <h2>search movies</h2>
-      
+      <input
+        type='text '
+        className='form-input'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
       {error.show && <div className='error'>{error.msg}</div>}
     </form>
   )
