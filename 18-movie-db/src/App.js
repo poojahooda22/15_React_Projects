4fe9781a2c4c9e0a5380f385d47e88e4eb6b8635
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-05-27 22:10:27 +0530

    second commit

--- 18-movie-db/src/App.js
+++ 18-movie-db/src/App.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import { Routes, Route } from 'react-router-dom';
 
-import Home from './Home';
-import Movie from './SingleMovie';
+// import Home from './Home';
+// import Movie from './SingleMovie';
 
-function App() {
+// function App() {
+//   return (
+//     <Routes>
+//       <Route path='/' element={<Home />} />
+//       <Route path='movies/:id' element={<Movie />} />
+//     </Routes>
+//   );
+// }
+
+// export default App;
+
+
+/* useReducer hook */
+import React, {useReducer} from "react";
+
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case"INCREMENT":
+      return {count: state.count + 1, showText: state.showText};
+    case "toggleSHowText":
+      return {count: state.count, showText: !state.showText};
+    default: 
+      return state;
+  }
+
+};
+
+const App = () => {
+
+  const [state, dispatch] = useReducer(reducer, {count: 0, showText: true});
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='movies/:id' element={<Movie />} />
-    </Routes>
-  );
+    <div>
+      <h1>{state.countt}</h1>
+
+      <button
+      onClick = {() => {
+        dispatch({type: "INCREMENT"});
+        dispatch({type: "toggleShowText"});
+      }}
+      >
+        Click Here
+        </button>
+        {state.showText && <p>This is a text</p>}
+    </div>
+  )
 }
 
 export default App;
